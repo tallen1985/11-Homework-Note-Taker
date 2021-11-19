676fb08ace77232e021e0085aee806ff0451b945
Troy Allen <jake.allen@me.com>
2021-11-18 23:07:15 -0500

    functional add

--- server.js
+++ server.js
@@ -1,21 +1,41 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path')
-const db = require('./db/db.json')
 
 const app = express();
 const PORT = 3001;
+const rawData = fs.readFileSync('./db/db.json', 'utf8')
+    let db = JSON.parse(rawData)
 
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
 app.get('/api/notes', (req, res) => {
     console.log(db)
     res.json(db)
+    res.status(200).send('notes loaded')
 })
 
 app.post('/api/notes', (req, res) => {
-    console.log('request is ' + req.body)
-})
+    const {title, text} = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: Date.now().toString()
+        }
+
+        db.push(newNote)
+
+        const stringDb = JSON.stringify(db);
+        fs.writeFileSync('./db/db.json', stringDb)
+        
+        console.log(`${newNote.title} added to list`)
+        res.status(200).send('note added successfully');
+        }
+    })
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 })
